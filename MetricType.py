a0dcf2f1fa1614dbceeb7c79c6c1a07be4f82e0f
spenceralbrecht <s.spencer.a@gmail.com>
2017-10-15 15:19:49 -0700

    Fixes indentation

--- MetricType.py
+++ MetricType.py
@@ -1,6 +1,6 @@
 from enum import Enum
 class MetricType(Enum):
-  BATTERY = 'battery'
-  CABIN = 'cabin'
-  SOLAR_CELL = 'solar_cell'
-  RPM = 'RPM'
+    BATTERY = 'battery'
+    CABIN = 'cabin'
+    SOLAR_CELL = 'solar_cell'
+    RPM = 'RPM'
