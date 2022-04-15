c2089160ab0e8ce4c2a8b064c261c1f6efc62d1a
Fei Ren <feir@usc.edu>
2022-04-14 18:54:31 -0700

    showMaximized

--- telemetry-pi/driver/IDriver.py
+++ telemetry-pi/driver/IDriver.py
@@ -1,16 +1,14 @@
 from abc import ABCMeta, abstractmethod
 
+
 class IInterface:
     __metaclass__ = ABCMeta
+
     @abstractmethod
-    def getBatteryTemperature(self,index): raise NotImplementedError
+    def getBatteryTemperature(self, index): raise NotImplementedError
 
     @abstractmethod
     def getRPM(self): raise NotImplementedError
 
     @abstractmethod
     def getConnection(self): raise NotImplementedError
-
-
-
-
