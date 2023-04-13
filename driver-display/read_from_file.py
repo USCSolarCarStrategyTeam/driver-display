2dea7da9a94c8146e2921d34a9195e4d74b58782
Fei Ren <feir@usc.edu>
2023-04-12 20:55:20 -0700

    adding test stuff for file reading

--- driver-display/read_from_file.py
+++ driver-display/read_from_file.py
@@ -1,8 +1,18 @@
+# generating random inputs
+# import random
+#
+# file = open('test_input.txt', 'w')
+# for i in range(1000):
+#     type = random.randint(0, 5)
+#
+#     value = random.randint(0, 99)
+#
+#     file.writelines(str(type) + ' ' + str(value) + '\n')
 
 file = open('test_input.txt', 'r')
 line = file.readline()
 
 while line:
-    print(line)
+    print(line.strip())
     line = file.readline()
 file.close()
